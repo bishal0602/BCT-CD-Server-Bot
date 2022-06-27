ea84faac1f13e74ab9e9b975a959ec60e4ece14b
bishal0602 <bishalpanta60@gmail.com>
2022-06-27 21:42:11 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const DiscordJS = require("discord.js");
+const { Intents } = DiscordJS;
+const WOKCommands = require("wokcommands");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const client = new DiscordJS.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
+
+client.on("ready", async () => {
+  client.user.setActivity("BCT at pulchowk", {
+    type: "PLAYING",
+  });
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, "commands"),
+    // featuresDir: path.join(__dirname, "features"),
+    testServers: process.env.TEST_SERVERS,
+    botOwners: process.env.BOT_OWNERS,
+    // mongoUri: process.env.DB_CONNECT,
+    disabledDefaultCommands: [
+      "help",
+      "channelonly",
+      "command",
+      "language",
+      "prefix",
+      "requiredrole",
+      "slash",
+    ],
+  }).setDefaultPrefix("$");
+});
+
+client.login(process.env.BOT_TOKEN);
---
+++ routineData.js
@@ -0,0 +1,169 @@
+const sunday = [
+  {
+    period: "1st Period",
+    subject: "Physics",
+    teacher: "GNL",
+    time: "10:15 - 11:55",
+  },
+  {
+    period: "2nd Period",
+    subject: "Comp. Programming",
+    teacher: "GG",
+    time: "11:55-12:45",
+  },
+  {
+    period: "Break",
+    time: "12-45 - 1:35",
+  },
+  {
+    period: "3rd Period",
+    subject: "Comp. Programming",
+    teacher: "GG",
+    time: "1:35-2:25",
+  },
+  {
+    period: "4th Period",
+    subject: "Comp. Programming Lab [Group D]",
+    time: "2:25-4:55",
+  },
+  {
+    period: "4th Period",
+    subject: "Physics Lab [Group C]",
+    time: "3:15-4:55",
+  },
+];
+
+const monday = [
+  {
+    period: "1st Period",
+    subject: "Basic Electrical Lab (Alternate Week)",
+    time: "10:15 - 12:45",
+  },
+];
+
+const tuesday = [
+  {
+    period: "1st Period",
+    subject: "Basic Electrical",
+    teacher: "SK",
+    time: "10:15 - 11:55",
+  },
+  {
+    period: "2nd Period",
+    subject: "Applied Mechanics",
+    teacher: "GSB",
+    time: "11:55-12:45",
+  },
+  {
+    period: "Break",
+    time: "12-45 - 1:35",
+  },
+  {
+    period: "3rd Period",
+    subject: "Drawing(Group C)",
+    time: "1:35-4:55",
+  },
+  {
+    period: "3rd Period",
+    subject: "Physics Lab [Group D]",
+    time: "3:15-4:55",
+  },
+];
+
+const wednesday = [
+  {
+    period: "1st Period",
+    subject: "Basic Electrical",
+    teacher: "SK",
+    time: "10:15 - 11:55",
+  },
+  {
+    period: "2nd Period",
+    subject: "Physics",
+    teacher: "KP/GNL",
+    time: "11:55-12:45",
+  },
+  {
+    period: "Break",
+    time: "12-45 - 1:35",
+  },
+  {
+    period: "3rd Period",
+    subject: "Math",
+    teacher: "HG",
+    time: "1:35-3:25",
+  },
+  {
+    period: "4th Period",
+    subject: "Comp. Programming",
+    teacher: "GG",
+    time: "2:25-3:15",
+  },
+  {
+    period: "5th Period",
+    subject: "Applied Mechanics",
+    teacher: "AK",
+    time: "3:15-4:55",
+  },
+];
+
+const thursday = [
+  {
+    period: "1st Period",
+    subject: "Math",
+    teacher: "GG",
+    time: "10:15 - 11:05",
+  },
+  {
+    period: "2nd Period",
+    subject: "Physics",
+    teacher: "KP",
+    time: "11:05-12:45",
+  },
+  {
+    period: "Break",
+    time: "12-45 - 1:35",
+  },
+  {
+    period: "3rd Period",
+    subject: "Comp. Programming Lab [Group C]",
+    time: "1:35-4:05",
+  },
+  {
+    period: "3rd Period",
+    subject: "Drawing [Group D]",
+    time: "1:35-4:55",
+  },
+];
+const friday = [
+  {
+    period: "1st Period",
+    subject: "Math",
+    teacher: "SD",
+    time: "10:15 - 11:55",
+  },
+  {
+    period: "2nd Period",
+    subject: "Math",
+    teacher: "PA",
+    time: "11:55-12:45",
+  },
+  {
+    period: "Break",
+    time: "12-45 - 1:35",
+  },
+  {
+    period: "3rd Period",
+    subject: "Applied Mechanics",
+    teacher: "GSB",
+    time: "1:35-3:15",
+  },
+  {
+    period: "4th Period",
+    subject: "Applied Mechanics",
+    teacher: "GSB",
+    time: "3:15-4:55",
+  },
+];
+
+module.exports = { sunday, monday, tuesday, wednesday, thursday, friday };
